82ad54bcae3e5cb967028e6d7083f3360185b2b5
Nik Olson <pizzascripters@gmail.com>
2019-05-17 16:34:08 -0500

    New Base Project

--- src/main/java/frc/robot/Main.java
+++ src/main/java/frc/robot/Main.java
@@ -9,14 +9,14 @@ package frc.robot;
 
 import edu.wpi.first.wpilibj.RobotBase;
 
+import java.io.IOException;
+
 public final class Main {
 
     private Main() {
-
     }
 
-    public static void main(String... args) {
+    public static void main(String... args){
         RobotBase.startRobot(Robot::new);
     }
-
 }
